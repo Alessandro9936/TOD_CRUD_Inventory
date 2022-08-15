532b26b3b1df4bdd8a42c6963f5a954d4fa82ff5
Alessandro <alecrinella99@gmail.com>
2022-08-15 08:07:20 +0200

    Create routes/controllers skeleton

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,3 +1,27 @@
 const Author = require("../models/author");
 const Story = require("../models/story");
 const Category = require("../models/category");
+
+// display all categories
+exports.category_list = (req, res) => {};
+
+// display detail of a specific category
+exports.category_detail = (req, res) => {};
+
+// display category create form on GET
+exports.category_create_get = (req, res) => {};
+
+// handle create category on POST
+exports.category_create_post = (req, res) => {};
+
+// display category update form on GET
+exports.category_update_get = (req, res) => {};
+
+// handle update category on POST
+exports.category_update_post = (req, res) => {};
+
+// display category delete form on GET
+exports.category_delete_get = (req, res) => {};
+
+// handle delete category on POST
+exports.category_delete_post = (req, res) => {};
